27a3251ef90a391267d78c396a1f7d4436cad31e
lunu <fairus200599@gmail.com>
2021-12-03 07:59:34 +0300

    gave good variable names in block

--- lesson_2/1.rb
+++ lesson_2/1.rb
@@ -7,6 +7,6 @@ days_in_february = String(gets.chomp).downcase == 'да' ? 29 : 28
 months = {  'Январь' => 31, 'Февраль' => days_in_february, 'Март' => 31, 'Апрель' => 30, 'Май' => 31, 'Июнь' => 30,
             'Июль' => 31, 'Август' => 31, 'Сентябрь' => 30, 'Октябрь' => 31, 'Ноябрь' => 30, 'Декабрь' => 31 }
 
-months.each do |k, v|
-  puts k if v == 30
+months.each do |month, days|
+  puts month if days == 30
 end
