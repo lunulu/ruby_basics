d8a37e9c676c92bfedde9d4d4f8b766d52ecd79f
lunu <fairus200599@gmail.com>
2021-12-07 17:49:59 +0300

    add railroad class pre-alpha version

--- lesson5/railroad.rb
+++ lesson5/railroad.rb
@@ -13,16 +13,11 @@ require_relative 'passenger_wagon'
 class RailRoad
   attr_reader :stations, :routes, :trains
 
-  def seed
-    self.stations = create_stations
-    self.routes = create_routes(stations)
-    self.trains = create_trains
-    [stations, routes, trains]
-  end
-
   def app
     puts 'Добро пожаловать в программу управления железнодорожными сетями.'
-    puts 'Здесь можно создавать станции, маршруты и поезда.'
+    self.stations = []
+    self.routes = []
+    self.trains = []
     loop do
       info
       case gets.chomp
@@ -30,7 +25,8 @@ class RailRoad
       when '2' then routes_app
       when '3' then trains_app
       when '4' then control_app
-      when 'exit' then break
+      when 'seed' then seed
+      when 'выход' then break
       else
         puts 'Неверный ввод.'
         sleep 1
@@ -43,14 +39,78 @@ class RailRoad
 
   attr_writer :stations, :routes, :trains
 
+  # Внутренние части приложения, которые должны быть доступны только внутри класса
   def stations_app
-    # Добавление станций
+    puts 'Здесь можно создавать станции'
+    puts 'Введите название станции и нажмите Enter, чтобы добавить станцию'
+    puts '\'станции\', чтобы получить список добавленных станций'
+    puts '\'назад\', чтобы вернуться в главное меню'
+    loop do
+      input = gets.chomp.downcase
+      case input
+      when 'назад' then break
+      when 'выход' then break
+      when 'станции' then stations.each { |station| puts station.name }
+      else stations << Station.new(input.capitalize)
+      end
+    end
   end
 
   def routes_app
-    # Создание маршрутов
-    # Добавление станций
-    # Удаление станций
+    puts 'Здесь можно создавать маршруты, добавлять и удалять промежуточные станции'
+    puts '1 - Добавить новый маршрут'
+    puts '2 - Добавить станцию в существующий маршрут'
+    puts '3 - Удалить станцию из существующего маршрута'
+    puts '\'маршруты\' - Отобразить существующие маршруты'
+    puts '\'назад\', чтобы вернуться в главное меню'
+    loop do
+      input = gets.chomp.downcase
+      case input
+      when 'назад' then break
+      when 'выход' then break
+      when 'маршруты' then routes.each { |route| puts "#{route.starting_station.name} - #{route.end_station.name}" }
+      when '1'
+        puts 'Список существующих станций:\n'
+        stations.each { |station| puts station.name }
+        puts 'Введите название начальной станции:'
+        station1 = stations.select { |station| station.name == gets.chomp.capitalize }.first
+        puts 'Введите название конечной станции:'
+        station2 = stations.select { |station| station.name == gets.chomp.capitalize }.first
+        routes << Route.new(station1, station2)
+        puts "Маршрут #{routes.last.starting_station.name} - #{routes.last.end_station.name} добавлен."
+      when '2'
+        puts 'Список существующих станций:'
+        stations.each { |station| puts station.name }
+        puts 'Выберите маршрут, в который хотите добавить промежуточную станцию:'
+        i = 1
+        routes.each do |route|
+          puts "#{i}: #{route.starting_station.name} - #{route.end_station.name}"
+          i += 1
+        end
+        route_number = gets.chomp.to_i
+        puts 'Введите название промежуточной станции:'
+        way_station = stations.select { |station| station.name == gets.chomp.capitalize }.first
+        routes[route_number - 1].add_way_station(way_station)
+        puts "Промежуточная станция #{way_station} добавлена"
+      when '3'
+        puts 'Выберите маршрут, в который хотите удалить промежуточную станцию:'
+        i = 1
+        routes.each do |route|
+          puts "#{i}: #{route.starting_station.name} - #{route.end_station.name}"
+          i += 1
+        end
+        route_number = gets.chomp.to_i - 1
+        puts 'Список промежуточных станций этого маршрута:'
+        route = routes[route_number]
+        route.stations.pop
+        route.stations.shift
+        route.show_route_stations
+        puts 'Выберите станцию, которую хотите удалить:'
+        route.delete_way_station(gets.chomp.capitalize)
+        puts 'Промежуточная станция удалена'
+      else puts 'Неверный ввод.'
+      end
+    end
   end
 
   def trains_app
@@ -67,12 +127,23 @@ class RailRoad
 
   def info
     puts 'Наберите на клавитуре и нажмите Enter, чтобы выбрать действие:'
-    puts 'С чем вы хотите работать?'
     puts '1 - Станции'
     puts '2 - Маршруты'
     puts '3 - Поезда'
     puts '4 - Управление движением'
-    puts 'exit - Выйти из программы'
+    puts 'выход - Выйти из программы'
+  end
+
+  # Автоматически создает станции, маршруты и поезда
+  def seed
+    self.stations = create_stations
+    self.routes = create_routes(stations)
+    self.trains = create_trains
+    system 'clear'
+    puts 'Seed успешно активирован.'
+    sleep 3
+    system 'clear'
+    [stations, routes, trains]
   end
 
   def create_stations
