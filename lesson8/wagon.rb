fe78801c0ecec98fc6875ada6c37cc7a8aa54fcb
lunu <fairus200599@gmail.com>
2021-12-15 20:04:58 +0300

    add wagon number autoset

--- lesson8/wagon.rb
+++ lesson8/wagon.rb
@@ -5,13 +5,19 @@ class Wagon
   include CompanyManufacturerInfo
   include ValidCheck
 
-  attr_reader :type
+  attr_reader :type, :number
+
+  @@number = 1
 
   def initialize
+    @number = @@number
+    @@number += 1
     validate!
   end
 
   protected
+
+  attr_writer :number
 
   def validate!
     raise 'Type has invalid format' if type != 'пассажирский' && type != 'грузовой'
