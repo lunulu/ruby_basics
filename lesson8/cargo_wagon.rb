4e30753a6238804bfc9923efca34b143c7c4e76f
lunu <fairus200599@gmail.com>
2021-12-15 16:57:14 +0300

    add cargo wagon features

--- lesson8/cargo_wagon.rb
+++ lesson8/cargo_wagon.rb
@@ -2,8 +2,29 @@
 
 # Грузовой вагон
 class CargoWagon < Wagon
-  def initialize
+  attr_reader :total_volume, :volume_occupied
+
+  def initialize(total_volume)
     @type = 'грузовой'
+    @total_volume = total_volume
+    @volume_occupied = 0
+    super()
+  end
+
+  def occupy_volume(volume)
+    self.volume_occupied += volume if volume_occupied + volume <= total_volume
+  end
+
+  def free_volume
+    total_volume - volume_occupied
+  end
+
+  protected
+
+  attr_writer :total_volume, :volume_occupied
+
+  def validate!
     super
+    raise 'The volume cannot be negative' if total_volume.negative?
   end
 end
