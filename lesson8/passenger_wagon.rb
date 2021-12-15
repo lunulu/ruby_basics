6fc337588cc551d99a4bea8480acf405e6f1e431
lunu <fairus200599@gmail.com>
2021-12-15 16:47:45 +0300

    add passenger wagon features

--- lesson8/passenger_wagon.rb
+++ lesson8/passenger_wagon.rb
@@ -2,8 +2,29 @@
 
 # Пассажирский вагон
 class PassengerWagon < Wagon
-  def initialize
+  attr_reader :number_of_seats, :seats_occupied
+
+  def initialize(number_of_seats)
     @type = 'пассажирский'
+    @number_of_seats = number_of_seats
+    @seats_occupied = 0
+    super()
+  end
+
+  def take_a_seat
+    self.seats_occupied += 1 if seats_occupied < number_of_seats
+  end
+
+  def free_seats
+    number_of_seats - seats_occupied
+  end
+
+  protected
+
+  attr_writer :number_of_seats, :seats_occupied
+
+  def validate!
     super
+    raise 'The number of seats cannot be negative' if number_of_seats.negative?
   end
 end
