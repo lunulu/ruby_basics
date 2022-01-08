bd257095c92f1e452d0cb60c9dcfaa436a91aa42
lunu <fairus200599@gmail.com>
2022-01-08 16:14:09 +0300

    major fix

--- lesson10/validation.rb
+++ lesson10/validation.rb
@@ -7,31 +7,56 @@ module Validation
     base.include InstanceMethods
   end
 
-  # Class Methods
+  # Class methods
   module ClassMethods
-    def validate(attr, validation_type, addition = nil)
-      name = instance_variable_get("@#{attr}".to_sym)
-      case validation_type
-      when :presence then raise 'Invalid presence' if name.nil? || name == ''
-      when :format then raise 'Invalid format' if name !~ addition
-      when :type then raise 'Invalid type' unless name.is_a? addition
-      end
+    def validate(var_name, validation_type, params = nil)
+      @validations ||= []
+      @validations << { var_name: var_name, validation_type: validation_type, params: params }
     end
   end
 
   # Instance methods
   module InstanceMethods
-    def validate!
-      self.class.validate :name, :presence
-      self.class.validate :name, :format, self.class.const_get(:FORMAT)
-      self.class.validate :name, :type, self.class.const_get(:TYPE)
-    end
-
     def valid?
       validate!
       true
     rescue RuntimeError
       false
     end
+
+    private
+
+    def validate!
+      validations = self.class.instance_variable_get(:@validations)
+      validations.each do |validation|
+        send "validate_#{validation[:validation_type]}", validation[:var_name], validation[:params]
+      end
+    end
+
+    def validate_presence(var, _)
+      raise 'Имя не может быть пустой строкой или nil.' if ['', nil].include? var
+    end
+
+    def validate_format(var, format)
+      raise 'Переменная не соответствует заданному формату' if var !~ format
+    end
+
+    def validate_type(var, type)
+      raise 'Переменная не соответствует заданному типу' unless var.is_a? type
+    end
+  end
+end
+
+class Test
+  include Validation
+
+  validate :name, :presence
+  validate :age, :type, Integer
+  validate :phone, :format, /^\d{7}$/
+
+  def initialize
+    @name = 'aaaa'
+    @age = 10
+    @phone = '1234567'
   end
 end
